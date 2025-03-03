14a9db3e59316eea614fc2591c20f2917e97f7ee
Toshipy <arai1209devops@gmail.com>
2025-03-03 23:03:05 +0900

    biome format

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,13 +1,9 @@
-import { ReactNode } from "react"
+import { ReactNode } from 'react'
 
 export default function AboutLayout({
   children
 }: {
   children: ReactNode
 }) {
-  return (
-    <div className="bg-slate-200 h-screen">
-      {children}
-    </div>
-  )
+  return <div className="bg-slate-200 h-screen">{children}</div>
 }
